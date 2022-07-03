f422fa2915f5cb7b9f792d01dc068bb73a2f0425
Rafael Hansen <hansenre@hotmail.com>
2022-07-03 15:12:29 -0300

    Criando a pasta partials e configurando o Header e footer

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 
 app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'..','public')));
 
 app.get('/', (request, response) => {
