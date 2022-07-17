2946abb271c9f5147697334d657cd6d3e69906ba
Rafael Hansen <hansenre@hotmail.com>
2022-07-17 16:28:37 -0300

    using some <% %> on ejs

--- src/controllers/clientecontroller.js
+++ src/controllers/clientecontroller.js
@@ -1,9 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const cadastroFilePath = path.join(__dirname, '..', 'data', 'cadastroDataBase.json');
+
 const clientecontroller = {
     cliente: (request, response) => {
         return response.redirect ('/login');
     },
     acesso: (request, response)=>{
-        return response.render('areacliente');
+        const cadastro = JSON.parse(fs.readFileSync(cadastroFilePath, 'utf-8'));
+        const cadastroCliente = cadastro.map((cliente)=>{
+            return{
+                ...cliente
+            }
+        });
+
+        return response.render('areacliente', { cadastroCliente });
     }
 }
 module.exports = clientecontroller;
